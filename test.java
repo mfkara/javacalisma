bd233c2c10acab5c9be8df8a016aa53aecdeb721
mfkara <zkara540@gmail.com>
2014-05-09 04:27:12 +0300

    Graphics Sınıfı Kullanımı

---
+++ test.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import gui.Sekilgui;
+
+/**
+ *
+ * @author Sungur Fırat
+ */
+public class test {
+    
+     public static void main(String[] args) {
+         System.out.println("test çalıs");
+         new Sekilgui().setVisible(true);
+    }
+}
